eb5a7d1fcf161ba6d0972f8d69951f855b225b80
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 11:53:26 +0800

    Create jobsheet 6 C++.cpp
    program menampilkan angka 9 s/d 0 dengan perulangan.

---
+++ jobsheet 6 C++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <math.h>
+#include<stdio.h>
+using namespace std;
+void contoh(){
+for(int i = 9;i>-1;i--){
+cout<<"\nAngka "<<i;
+}
+}
+int main(){
+contoh();// cara pemanggilan fungsi
+}
